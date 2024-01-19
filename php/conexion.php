06ffed832d3d993c8e9226c6a2b12185fd17fb42
Wendy Portilla <wendyportilla13@gmail.com>
2024-01-19 07:50:06 -0500

    proyecto

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $db_host='localhost';
 $db_usuario='root';
-$db_password='';
+$db_password='Itsqmet2';
 $db_nombre='tallerpractico';
 
 $conexion=mysqli_connect($db_host,$db_usuario,$db_password,$db_nombre);
